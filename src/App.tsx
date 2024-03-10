a6f29f7a7ef9e50b1f211614ae78ea5584f6dae6
Megan Lake <meganlake95@gmail.com>
2024-03-10 16:54:05 -0700

    linked photo

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Main from './components/MainComponent'
 import Footer from './components/FooterComponent'
 import './App.css'
-import Pic from './components/travel.jpg'
 
 function App() {
   return (
@@ -10,7 +9,7 @@ function App() {
       <h1>Hello Fellow Travelers! Welcome!</h1>
       <Main />
       <Footer />
-      <img src={Pic} alt='travel' />
+      <img src='https://images.unsplash.com/photo-1488646953014-85cb44e25828?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='travel' />
     </div>
   );
 }
